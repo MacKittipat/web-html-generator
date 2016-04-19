1d114636eade07d0eba086df7e4e7a18b70b14e1
Mac <mac@abctech-thailand.com>
2016-04-19 22:42:19 +0700

    Generate output file in out dir

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from jinja2 import Template
+import os
 
+# Read HTML template and render using Jinja2
 file = open('src/index.html', 'r')
-
 template = Template(file.read())
-print template.render({
+templateRendered = template.render({
     'title': 'Mac Java'
 })
 
-# file = open("newfile.txt", "w")
-# file.write("test")
-# file.close()
+# Create out dir if it does not exist
+outDir = 'out/'
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+# Write output file
+file = open("out/index.html", "w")
+file.write(templateRendered)
+file.close()
